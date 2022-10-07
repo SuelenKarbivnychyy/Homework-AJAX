e35084ce16344b670595aced97cb8446c9a4749a
suelenk <suelenmatosr@gmail.com>
2022-10-07 12:49:29 -0400

    added comments

--- static/profile.js
+++ static/profile.js
@@ -4,28 +4,28 @@ function submitProfile(evt) {
   const data = {
     name: document.querySelector('#name-field').value,
     // fill in the rest
-    age: document.getElementById('age-field').value,
-    occupation: document.getElementById('occupation-field').value
+    age: document.getElementById('age-field').value,                  //getting info from server, using the same variable got from json on the python side(server)
+    occupation: document.getElementById('occupation-field').value     //getting info from server, using the same variable got from json on the python side(server)
   };
 
   // make request to server to get the data
 
   fetch('/api/profile', {
     method: 'POST',
-    body: JSON.stringify(data),
+    body: JSON.stringify(data),                                         // making the data json string
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/json',                              // specifying that it will be json file
     },
   })
   // add the data the server returns to the document
   .then((response) => response.json())
   .then((responseJson) => {
-    //create nice html from json
-    let name = responseJson['fullname'];
-    let age = responseJson['customer_age'];
-    let occupation = responseJson['customer_ocupation'];
+   
+    let name = responseJson['fullname'];                            //creating a nice html from json
+    let age = responseJson['customer_age'];                        //creating a nice html from json
+    let occupation = responseJson['customer_ocupation'];          //creating a nice html from json
 
-    document.getElementById('profiles').innerHTML += `<p> ${name}, ${age}, ${occupation}`;
+    document.getElementById('profiles').innerHTML += `<p> ${name}, ${age}, ${occupation}`; // adding data to div as content using innerHTML
   })
   // (you can add it to the end of the div with ID "profiles")
 }
